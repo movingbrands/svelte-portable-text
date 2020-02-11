e53ea6a53b434287287aa9b9910e3590e7759fd7
Toby Milner-Gulland <49916680+tmgulland@users.noreply.github.com>
2020-02-11 10:03:49 +0000

    another approach to rendering/container issue

--- src/index.svelte
+++ src/index.svelte
@@ -1,9 +1,16 @@
 <script>
   import blockContentToNodes from "./blockContentToNodes";
   import ChildNode from "./ChildNode.svelte";
+  import ChildNodes from "./ChildNodes.svelte";
 
   export let blocks = [];
   export let serializers = {};
+
+  export const removeChildNodes = obj => {
+    const result = Object.assign({}, obj);
+    delete result.childNodes;
+    return result;
+  };
 
   $: converted = blockContentToNodes({
     blocks,
@@ -11,13 +18,8 @@
   });
 </script>
 
-{#if converted.childNodes && converted.childNodes.length === 1}
+{#if blocks.length === 1}
   <ChildNode {...converted} />
 {:else}
-  <ChildNode {...converted} />
-  {#each converted.childNodes as childNode}
-    {#if childNode}
-      <ChildNode {...childNode} />
-    {/if}
-  {/each}
+  <ChildNodes childNodes={converted.childNodes} />
 {/if}
